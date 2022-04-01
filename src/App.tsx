af1a8c72bb27e92a4b2c55b93c1f458cbcc4d23b
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2022-04-01 18:45:23 +0100

    add hero section

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
 import React from "react";
+import Hero from "./Sections/Hero";
 
 function App() {
-  return <div className="App"></div>;
+  return <div className="App">
+    <Hero/>
+  </div>;
 }
 
 export default App;
---
+++ src/Sections/Hero.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { CgChevronDown } from "react-icons/cg";
+
+const Hero = () => {
+  return (
+    <div
+      className="flex flex-col items-center justify-center w-full h-screen gap-20 font-[400] text-[#161616] []"
+      style={{
+        fontSize: "clamp(1rem, 7vw, 5rem)",
+      }}
+    >
+      <div className="flex flex-col justify-center items-left">
+        <p className="text-sm"> Hey, </p>
+        <p>I'm Mohamed Bechir,</p>
+        <p>A full-stack developer</p>
+        <p>based in Tunisia ❤️</p>
+      </div>
+      <p className=" animate-[bounce_1.5s_linear_infinite]">
+        <CgChevronDown className="text-3xl" />
+      </p>
+    </div>
+  );
+};
+
+export default Hero;
