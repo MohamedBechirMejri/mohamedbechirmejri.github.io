9374a6d14e71f6c6652a47fa6a6f10f011874c44
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2022-04-03 16:02:46 +0100

    add skills component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from "react";
-import About from "./Sections/About";
-import Hero from "./Sections/Hero";
+import About from "./Components/Sections/About";
+import Hero from "./Components/Sections/Hero";
+import Skills from "./Components/Sections/Skills";
 
 function App() {
   return <div className="App">
     <Hero />
     <About />
+    <Skills/>
   </div>;
 }
 
---
+++ src/Components/Sections/Skills.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import css3 from "../../Assets/Logos/css3.svg";
+import eslint from "../../Assets/Logos/eslint.svg";
+import git from "../../Assets/Logos/git.svg";
+import github from "../../Assets/Logos/github.svg";
+import heroku from "../../Assets/Logos/heroku.svg";
+import html5 from "../../Assets/Logos/html5.svg";
+import javascript from "../../Assets/Logos/javascript.svg";
+import jest from "../../Assets/Logos/jest.svg";
+import mongodb from "../../Assets/Logos/mongodb.svg";
+import nodejs from "../../Assets/Logos/nodejs.svg";
+import npm from "../../Assets/Logos/npm.svg";
+import rails from "../../Assets/Logos/rails.svg";
+import react from "../../Assets/Logos/react.svg";
+import ruby from "../../Assets/Logos/ruby.svg";
+import sass from "../../Assets/Logos/sass.svg";
+import tailwind from "../../Assets/Logos/tailwind.svg";
+import typescript from "../../Assets/Logos/typescript.svg";
+import webpack from "../../Assets/Logos/webpack.svg";
+import yarn from "../../Assets/Logos/yarn.svg";
+
+const Skills = () => {
+  return (
+    <div>
+      <h1>Skills</h1>
+
+      {css3}
+    </div>
+  );
+};
+
+export default Skills;
