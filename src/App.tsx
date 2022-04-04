d6228fe81e6ae3a9f399e0a38653e51173550950
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2022-04-04 23:16:05 +0100

    remove bugs

--- src/App.tsx
+++ src/App.tsx
@@ -3,17 +3,14 @@ import About from "./Components/Sections/About";
 import Hero from "./Components/Sections/Hero";
 import Skills from "./Components/Sections/Skills";
 import Header from "./Components/Header";
-import SmoothScroll from "./Components/SmoothScroll/SmoothScroll.jsx";
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <SmoothScroll>
-        <Hero /> 
-        <About />
-        <Skills />
-      </SmoothScroll>
+      <Hero />
+      <About />
+      <Skills />
     </div>
   );
 }
