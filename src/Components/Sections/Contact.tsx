4e89159d6c6b6983fea0838b67db3ad651b2acf1
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2022-04-05 16:19:46 +0100

    style contact section

--- src/Components/Sections/Contact.tsx
+++ src/Components/Sections/Contact.tsx
@@ -2,19 +2,24 @@ import React from "react";
 
 const Contact = () => {
   return (
-    <div>
-      <h1>Contact</h1>
-      <p>
-        You can contact me at{" "}
-        <a
-          href="mailto:mohamedbechir.mejri.dev@gmail.com
+    <div className="p-8 bg-[#afafaf] h-screen w-full flex flex-col items-center justify-center">
+      <h1 className="w-full text-[clamp(3rem,6vw,4rem)] text-center font-[400] p-4">
+        Get in touch
+      </h1>
+      <p className=" text-lg font-[300] text-center max-w-3xl p-4">
+        I'm currently looking for a role as a front-end/back-end web developer.
+        Whether you have a question, an offer or just want to chat, send me an
+        email and I will get back to you!
+      </p>{" "}
+      <a
+        href="mailto:mohamedbechir.mejri.dev@gmail.com
             "
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          here
-        </a>
-      </p>
+        target="_blank"
+        rel="noopener noreferrer"
+        className="px-8 py-4 m-4 transition-all rounded text-zinc-200 bg-zinc-800 hover:shadow-[0_0_0_8px] hover:bg-zinc-300 hover:text-zinc-800 font-bold shadow-[0_0_0_5px] active:shadow-[0_0_0_4px] "
+      >
+        Say hello!
+      </a>
     </div>
   );
 };
