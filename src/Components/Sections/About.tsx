c1877a930eaa04645c02240c0e056d3602e637ba
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2022-04-05 17:49:20 +0100

    limit paragraph size

--- src/Components/Sections/About.tsx
+++ src/Components/Sections/About.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const About = () => {
   return (
     <div
-      className="flex flex-col items-center justify-start w-full h-screen gap-12 px-12 py-24 text-lg text-[161616] "
+      className="flex flex-col items-center justify-center w-full h-screen gap-12 px-12 py-24 text-lg text-[161616] "
       style={{
         fontSize: "clamp(1rem, 3vw, 1.3rem)",
         fontWeight: 300,
@@ -13,7 +13,7 @@ const About = () => {
       <h1 className="w-full text-[clamp(3rem,1vw,4rem)] text-center font-[400] p-12 before:content-[''] before:bg-green-500 before:opacity-70 before:w-2/5 before:h-8 before:absolute before:left-1/2 before:-z-10 before:-rotate-[2deg] relative before:top-1/2 before:-translate-x-1/2 before:-translate-y-1/2 ">
         About Me
       </h1>
-      <p className="max-w-[80rem]">
+      <p className="max-w-[65rem] text-justify">
         <span
           style={{
             lineHeight: 1.1,
@@ -26,6 +26,7 @@ const About = () => {
           it looks or add some new features. <strong> problem solving </strong>
           and logic games were ( and still are ) my favorite past time.
         </span>
+        <br />
         <br />
         Right after I learned about web development<strong> I knew </strong>it
         was the right career for me and I started learning how to code in march
