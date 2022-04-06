aa0518c2f94be41add30ee1f3668e74f463a850d
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2022-04-06 03:48:23 +0100

    add custom cursor

--- src/App.tsx
+++ src/App.tsx
@@ -6,10 +6,25 @@ import Header from "./Components/Header";
 import Projects from "./Components/Sections/Projects";
 import Contact from "./Components/Sections/Contact";
 import SocialLinks from "./Components/SocialLinks";
+// @ts-ignore
+import CustomCursor from "custom-cursor-react";
+import "custom-cursor-react/dist/index.css";
 
 function App() {
   return (
     <div className="App">
+      <CustomCursor
+        targets={[".link", ".your-css-selector"]}
+        customClass="custom-cursor"
+        dimensions={30}
+        fill="#FFF"
+        smoothness={{
+          movement: 0.2,
+          scale: 0.1,
+          opacity: 0.2,
+        }}
+        targetOpacity={0.5}
+      />
       <Header />
       <SocialLinks />
       <Hero />
