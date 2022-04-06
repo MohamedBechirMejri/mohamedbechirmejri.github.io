de5fff0b736c650221d1c0420c86af64970a2f64
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2022-04-06 01:10:20 +0100

    add social links component

--- src/App.tsx
+++ src/App.tsx
@@ -5,11 +5,13 @@ import Skills from "./Components/Sections/Skills";
 import Header from "./Components/Header";
 import Projects from "./Components/Sections/Projects";
 import Contact from "./Components/Sections/Contact";
+import SocialLinks from "./Components/SocialLinks";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <SocialLinks />
       <Hero />
       <About />
       <Skills />
---
+++ src/Components/SocialLinks.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const SocialLinks = () => {
+  return <div></div>;
+};
+
+export default SocialLinks;
