cd4b626a644bb7e0cfcde24152525a7d3a793f48
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2022-05-26 09:19:55 +0100

    Add Loading text

--- src/Components/Preloader.tsx
+++ src/Components/Preloader.tsx
@@ -1,9 +1,19 @@
 import React from "react";
 
 const Preloader = () => {
+  const [loaded, setLoaded] = React.useState(false);
+
+  window.onload = () => {
+    setTimeout(() => {
+      setLoaded(true);
+    }, 1000);
+  };
   return (
     <div className="fixed z-50 w-screen h-screen bg-zinc-200">
-      <div className="absolute w-32 h-32 -translate-x-1/2 -translate-y-1/2 bg-green-500 rounded-full inset-1/2 rotate-animation"></div>
+      <div className="absolute -translate-x-1/2 -translate-y-1/2 bg-green-500 rounded-full h-36 w-36 inset-1/2 rotate-animation"></div>{" "}
+      <div className="absolute font-mono text-xl font-bold -translate-x-1/2 -translate-y-1/2 inset-1/2 w-max h-max text-zinc-200 animate-pulse">
+        Loading...
+      </div>
     </div>
   );
 };
