a8b707c86543f3aa43a37684afa3b71080ef8fe3
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2022-05-26 06:23:38 +0100

    improve button hover animation

--- src/Components/Sections/Contact.tsx
+++ src/Components/Sections/Contact.tsx
@@ -2,7 +2,10 @@ import React from "react";
 
 const Contact = () => {
   return (
-    <div className="p-8 bg-[#afafaf] h-screen w-full flex flex-col items-center justify-center" id="contact">
+    <div
+      className="p-8 bg-[#afafaf] h-screen w-full flex flex-col items-center justify-center"
+      id="contact"
+    >
       <h1
         className="w-full text-[clamp(3rem,6vw,4rem)] text-center font-[400] p-4 before:content-[''] before:bg-fuchsia-600 before:opacity-70 before:w-1/3 before:h-8 before:absolute before:left-1/2 before:-z-10 before:-rotate-[2deg] relative before:top-1/2 before:-translate-x-1/2 before:-translate-y-1/2 "
         style={{
@@ -22,7 +25,7 @@ const Contact = () => {
         target="_blank"
         rel="noopener noreferrer"
         // className="px-8 py-4 m-4 transition-all rounded text-zinc-200 bg-zinc-800 hover:shadow-[0_0_0_8px] hover:bg-zinc-300 hover:text-zinc-800 font-bold shadow-[0_0_0_5px] active:shadow-[0_0_0_4px] "
-        className="px-8 py-4 m-4 transition-all rounded text-zinc-300 bg-zinc-800  hover:text-zinc-800 font-bold active:shadow-[0_0_0_4px] relative overflow-hidden before:w-full before:h-full before:absolute before:top-0 before:transition-all before:-z-10 before:-left-full hover:before:left-0 before:bg-zinc-300 border-2 border-zinc-800 before:duration-[350ms] before:ease-in-out duration-[350ms]"
+        className="px-8 py-4 m-4 transition-all rounded text-zinc-300 bg-zinc-800 hover:text-zinc-800 font-bold active:shadow-[0_0_0_4px] relative overflow-hidden before:absolute before:transition-transform before:-z-10 before:inset-0 before:bg-zinc-300 border-2 border-zinc-800 before:duration-[350ms] before:ease-in-out duration-[350ms] hover:before:scale-x-100 hover:before:origin-left before:origin-right before:scale-x-0"
         style={{
           transformStyle: "preserve-3d",
         }}
